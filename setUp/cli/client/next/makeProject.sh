28ae165a5bdc8c0220f0b9aa33aba711cd2a6efc
Giovanny Alfonso Chávez Ceniceros <gio.icc.uach@gmail.com>
2023-05-25 17:23:15 -0600

    [main] modify to install storybook@7.0.12

--- setUp/cli/client/next/makeProject.sh
+++ setUp/cli/client/next/makeProject.sh
@@ -2,6 +2,6 @@
 appName=$1
 npx create-next-app@13.3.0 $appName --js --use-npm --src-dir --eslint --tailwind --no-experimental-app --import-alias "@/*" &> /dev/null
 cd ./$appName
-npm i -D eslint-plugin-storybook @storybook/addon-styling postcss-loader &> /dev/null
-npx storybook@next init &> /dev/null
+npm i -D eslint-plugin-storybook @storybook/addon-styling postcss-loader storybook@7.0.12 &> /dev/null
+npx storybook init &> /dev/null
 cd ..
